ce1e72accb8683cd7deaa25d6b2847606eb088b0
Rodr1go Garcia <gemeosgarcia@gmail.com>
2018-02-22 10:33:29 +0000

    Testar Strings

--- TiposVarios/Program.cs
+++ TiposVarios/Program.cs
@@ -10,6 +10,12 @@ namespace TiposVarios
     {
         static void Main(string[] args)
         {
+            string strn = "Um tab\t e um copyright\u03A9";
+            string sv = @"C:\\Users\\Ana\\Documents\u+0194";
+
+            Console.WriteLine(" " + strn + " ");
+            Console.WriteLine(" " + sv + " " );
+
         }
     }
 }
